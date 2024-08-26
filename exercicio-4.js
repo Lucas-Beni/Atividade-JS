e459c19e24ec57110bc7ee31d607e0e5ba4b9b68
Lucas-Beni <lucgarcbeni@gmail.com>
2024-08-26 18:42:18 -0300

    mudanças no ex 2, 3 e 4

--- exercicio-4.js
+++ exercicio-4.js
@@ -4,16 +4,16 @@ function calc() {
 
     if (select == "Bom"){
     let precoFinal = preco * 1.10
-    document.getElementById("resultado").innerHTML = "Sua refeição de valor " + preco + " com uma gorjeta de 10% fica em um valor total de " + precoFinal.toFixed(2) + "."
+    document.getElementById("resultado").innerHTML = "Sua refeição de valor R$ " + preco + " com uma gorjeta de 10% fica em um valor total de R$ " + precoFinal.toFixed(2) + "."
     }
 
     else if (select == "Ótimo"){
     let precoFinal = preco * 1.15
-    document.getElementById("resultado").innerHTML = "Sua refeição de valor " + preco + " com uma gorjeta de 15% fica em um valor total de " + precoFinal.toFixed(2) + "."
+    document.getElementById("resultado").innerHTML = "Sua refeição de valor R$ " + preco + " com uma gorjeta de 15% fica em um valor total de R$ " + precoFinal.toFixed(2) + "."
     }
 
     else if (select == "Excelente"){
     let precoFinal = preco * 1.20
-    document.getElementById("resultado").innerHTML = "Sua refeição de valor " + preco + " com uma gorjeta de 20% fica em um valor total de " + precoFinal.toFixed(2) + "."
+    document.getElementById("resultado").innerHTML = "Sua refeição de valor R$ " + preco + " com uma gorjeta de 20% fica em um valor total de R$ " + precoFinal.toFixed(2) + "."
     }
 }
