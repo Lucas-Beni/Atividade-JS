0793e8c72f86a2fe0069043116d6e13e06c4ef8a
Lucas Beni <lucgarcbeni@gmail.com>
2024-08-28 16:51:47 -0300

    exercicio 1 array fracassado

--- exercicios array/exercicio-1.js
+++ exercicios array/exercicio-1.js
@@ -1,4 +1,19 @@
 function somaArray(){
 
+    let numeros = []
+
+    let sla = document.getElementById("numero").value
+    let sla2 = sla.split(", ")
+    document.write(sla2)
+
+    let soma = 0
+
+
+
+    numeros.forEach(function(elemento){
+        soma += numeros
+    })
+
+    document.getElementById("resultado").innerHTML = `a soma de todos esses números é: ${soma}` 
     
 }
