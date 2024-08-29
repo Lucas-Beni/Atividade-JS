b5d1b2d3ea1c2c674d68df823fac7e9a54b5140b
Lucas-Beni <lucgarcbeni@gmail.com>
2024-08-29 00:08:43 -0300

    css da pagina 1 começado

--- exercicios array/exercicio-1.js
+++ exercicios array/exercicio-1.js
@@ -1,17 +1,11 @@
 function somaArray(){
 
-    let numeros = []
-
-    let sla = document.getElementById("numero").value
-    let sla2 = sla.split(", ")
-    document.write(sla2)
+    let numeros = [4, 9, 10, 15]
 
     let soma = 0
 
-
-
-    numeros.forEach(function(elemento){
-        soma += numeros
+    numeros.forEach(function(numero){
+        soma += numero
     })
 
     document.getElementById("resultado").innerHTML = `a soma de todos esses números é: ${soma}` 
