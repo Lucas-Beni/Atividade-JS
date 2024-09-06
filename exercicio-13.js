38169b530c2e99471f319b4512ac32a0a97cccf4
Lucas Beni <lucgarcbeni@gmail.com>
2024-09-06 16:48:43 -0300

    mais css

--- exercicio-13.js
+++ exercicio-13.js
@@ -1,11 +1,11 @@
 function verificar(){
     let num = parseInt(document.getElementById("num").value)
 
-    for (let q = 1; q < 2; q++) {
+    do {
         if (num % 2 === 0) {
             document.getElementById("resultado").innerHTML = "Este número é par!"
         } else {
             document.getElementById("resultado").innerHTML = "Este número é impar!"
         }
-    }
+    } while (0 === 1)
 }
